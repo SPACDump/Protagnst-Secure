324d938eb38b5f7d40af0ba9330682d83a14a2c3
Jack <55438332+InfinityJxck@users.noreply.github.com>
2022-11-15 00:39:27 +0000

    refactor(schema): Redo schema, bigger&better 😎

--- src/setup.sql
+++ src/setup.sql
@@ -1,7 +1,62 @@
-CREATE DATABASE IF NOT EXISTS protagnstsecure;
-USE protagnstsecure;
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
 
--- Users Table
-CREATE TABLE IF NOT EXISTS users (
-  discord_id varchar(255) PRIMARY KEY NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for protagnstsecure
+CREATE DATABASE IF NOT EXISTS `protagnstsecure` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `protagnstsecure`;
+
+-- Dumping structure for table protagnstsecure.forms
+CREATE TABLE IF NOT EXISTS `forms` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `form_name` varchar(255) NOT NULL,
+  `form_description` varchar(255) NOT NULL,
+  `permissions_needed` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table protagnstsecure.forms: ~0 rows (approximately)
+
+-- Dumping structure for table protagnstsecure.submissions
+CREATE TABLE IF NOT EXISTS `submissions` (
+  `discord_id` varchar(255) NOT NULL,
+  `submission_id` int NOT NULL AUTO_INCREMENT,
+  `form_id` int NOT NULL,
+  `submitted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `outcome` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`submission_id`,`discord_id`) USING BTREE,
+  KEY `form_id` (`form_id`),
+  KEY `discord_id` (`discord_id`),
+  CONSTRAINT `discord_id` FOREIGN KEY (`discord_id`) REFERENCES `users` (`discord_id`),
+  CONSTRAINT `form_id` FOREIGN KEY (`form_id`) REFERENCES `forms` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table protagnstsecure.submissions: ~0 rows (approximately)
+
+-- Dumping structure for table protagnstsecure.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `discord_id` varchar(30) NOT NULL,
+  `refresh_token` varchar(30) NOT NULL,
+  PRIMARY KEY (`discord_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table protagnstsecure.users: ~0 rows (approximately)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
