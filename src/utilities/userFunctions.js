10997c0cc2af7b7c9ddf0977c04acd45ad825396
Jack <55438332+InfinityJxck@users.noreply.github.com>
2022-12-26 22:17:28 +0000

    tweak(func): Add global helper to user functions

--- src/utilities/userFunctions.js
+++ src/utilities/userFunctions.js
@@ -1,9 +1,13 @@
 const { executeMysqlQuery } = require("./mysqlHelper");
 
-async function checkUserPermissions(discord_id) {
+async function globalCheckHelper(discord_id) {
     let user = await executeMysqlQuery(`SELECT * FROM users WHERE discord_id = ?`, [discord_id]);
 
-    if (user.length <= 0) return 0;
+    if (user.length <= 0) return false;
+
+    return user[0];
+}
+
 
     let userPermission = user[0].permission_level;
     return userPermission;
