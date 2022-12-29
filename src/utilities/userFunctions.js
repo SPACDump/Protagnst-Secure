91442916445d4c85667a84de207d2045bb3b5fa8
Jack <55438332+InfinityJxck@users.noreply.github.com>
2022-12-29 03:21:37 +0000

    feat(func): refreshAccessToken & putUserInGuild

--- src/utilities/userFunctions.js
+++ src/utilities/userFunctions.js
@@ -1,3 +1,4 @@
+const { encrypt } = require("./aes");
 const { executeMysqlQuery } = require("./mysqlHelper");
 
 async function globalCheckHelper(discord_id) {
@@ -29,4 +30,49 @@ async function getUserBanStatus(discord_id) {
     return user.is_banned || false;
 }
 
-module.exports = { checkUserPermissions, getUserMC, getUserBanStatus };
+async function refreshAccessToken(refresh_token, discord_id) {
+    let data = await fetch(`https://discord.com/api/oauth2/token`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            client_id: process.env.DISCORD_CLIENT_ID,
+            client_secret: process.env.DISCORD_CLIENT_SECRET,
+            grant_type: "refresh_token",
+            refresh_token: refresh_token,
+        })
+    });
+
+    let json = await data.json();
+
+    console.log(json.refresh_token);
+    let newRefreshToken = encrypt(json.refresh_token);
+    await executeMysqlQuery(`UPDATE users SET refresh_token = ? WHERE discord_id = ?`, [newRefreshToken, discord_id]);
+
+    return json.access_token;
+}
+
+async function putUserInGuild(access_token, user_id, guild_id, roles) {
+    let userMC = await getUserMC(user_id);
+
+    let data = await fetch(`https://discord.com/api/v10/guilds/${guild_id}/members/${user_id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bot ${process.env.BOT_TOKEN}`
+        },
+        body: JSON.stringify({
+            access_token: access_token,
+            nick: userMC,
+            roles: roles,
+        })
+    }).catch(err => {
+        console.log(err);
+        return false;
+    });
+
+    return true;
+}
+
+module.exports = { checkUserPermissions, getUserMC, getUserBanStatus, refreshAccessToken, putUserInGuild };
