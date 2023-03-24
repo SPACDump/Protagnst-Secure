caa0072f1cd17e8c401e10e792366dd33f5edc49
Jack <55438332+InfinityJxck@users.noreply.github.com>
2023-03-24 14:37:44 +0000

    wip fix for oauth2 tokens

--- src/utilities/userFunctions.js
+++ src/utilities/userFunctions.js
@@ -1,5 +1,7 @@
 const { encrypt } = require("./aes");
 const { executeMysqlQuery } = require("./mysqlHelper");
+const queryString = require('querystring')
+const axios = require('axios')
 
 async function globalCheckHelper(discord_id) {
     let user = await executeMysqlQuery(`SELECT * FROM users WHERE disc = ?`, [discord_id]);
@@ -20,7 +22,7 @@ async function getUserMC(discord_id) {
     let user = await globalCheckHelper(discord_id);
     if (!user) return "None";
     
-    return user.minecraft_name || "None";
+    return user.mc || "None";
 }
 
 async function getUserBanStatus(discord_id) {
@@ -33,9 +35,6 @@ async function getUserBanStatus(discord_id) {
 async function refreshAccessToken(refresh_token, discord_id) {
     let data = await fetch(`https://discord.com/api/oauth2/token`, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
         body: new URLSearchParams({
             client_id: process.env.DISCORD_CLIENT_ID,
             client_secret: process.env.DISCORD_CLIENT_SECRET,
@@ -55,7 +54,7 @@ async function refreshAccessToken(refresh_token, discord_id) {
 async function putUserInGuild(access_token, user_id, guild_id, roles) {
     let userMC = await getUserMC(user_id);
 
-    let data = await fetch(`https://discord.com/api/v10/guilds/${guild_id}/members/${user_id}`, {
+    let debug = await fetch(`https://discord.com/api/v10/guilds/${guild_id}/members/${user_id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
